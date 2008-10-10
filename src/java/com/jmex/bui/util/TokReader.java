f9d1fba350c8d70ae8e5ac8d2be621b6b42709a0
standtrooper <standtrooper@bc7b2f82-3230-0410-b470-b99ffb12e3d1>
2008-10-10 16:43:09 +0000

    TokReader to extract the StreamTokenizer functionality

---
+++ src/java/com/jmex/bui/util/TokReader.java
@@ -0,0 +1,26 @@
+/**
+ * $ID:$
+ * $COPYRIGHT:$
+ */
+package com.jmex.bui.util;
+
+import java.io.BufferedReader;
+import java.io.Reader;
+import java.io.StreamTokenizer;
+
+/**
+ * @author torr
+ * @since Oct 9, 2008 - 11:48:34 AM
+ */
+public class TokReader {
+    public static StreamTokenizer tokenize(final Reader reader) {
+        final StreamTokenizer tok = new StreamTokenizer(new BufferedReader(reader));
+        tok.lowerCaseMode(true);
+        tok.slashSlashComments(true);
+        tok.slashStarComments(true);
+        tok.eolIsSignificant(false);
+        tok.wordChars('#', '#');
+        tok.wordChars('_', '_');
+        return tok;
+    }
+}
