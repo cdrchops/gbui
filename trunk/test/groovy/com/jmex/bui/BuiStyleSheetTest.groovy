32753c084b60ea42ce3062a9b34f26e42a4ae2fb
standtrooper <standtrooper@bc7b2f82-3230-0410-b470-b99ffb12e3d1>
2008-10-10 22:04:32 +0000

    Test for using cssparser to retrieve tokens for css and turn them into a map of maps to pull as objects

--- trunk/test/groovy/com/jmex/bui/BuiStyleSheetTest.groovy
+++ trunk/test/groovy/com/jmex/bui/BuiStyleSheetTest.groovy
@@ -4,9 +4,11 @@
  */
 package com.jmex.bui
 
-import com.steadystate.css.parser.SACParserCSS2
-import com.steadystate.css.parser.SACParserCSS2TokenManager
-import com.steadystate.css.parser.CharStream
+import com.steadystate.css.parser.CSSOMParser
+import org.w3c.css.sac.InputSource
+import org.w3c.dom.css.CSSRule
+import org.w3c.dom.css.CSSStyleRule
+import org.w3c.dom.css.CSSStyleSheet
 
 /**
  *
@@ -15,17 +17,75 @@ import com.steadystate.css.parser.CharStream
  * @since Oct 8, 2008 - 4:11:03 PM
  *
  */
-class BuiStyleSheetTest extends GroovyTestCase {
-    void test() {
-        def s = new SACParserCSS2()
-        s.parseStyleSheet("file://../gbui/trunk/test/groovy/com/jmex/bui/colors.css")
 
-//        def f = new File("../gbui/trunk/build/rsrc/style2.bss")
-//        println ClassPath.getResourceAsStream("/rsrc/style2.bss")
-//        println f.absolutePath
+CSSOMParser parser = new CSSOMParser();
+def iss = BuiStyleSheetTest.getClass().getResourceAsStream("/rsrc/colors.css")
+def br = new BufferedReader(new InputStreamReader(iss))
+def is = new InputSource(br);
+CSSStyleSheet stylesheet = parser.parseStyleSheet(is, null, null);
 
-//        def bss = new BStyleSheet(new FileReader(f), new BStyleSheet.DefaultResourceProvider())
+def listing = [:]
 
+def rules = stylesheet.cssRules
+def size = rules.getLength()
 
+for (int i = 0; i < size; i++) {
+    CSSRule rule = rules.item(i);
+    switch (rule.getType()) {
+        case CSSRule.UNKNOWN_RULE:
+//                addUnknownRule(node, (CSSUnknownRule) rule);
+            println "CSSUnknownRule"
+            break;
+        case CSSRule.STYLE_RULE:
+//                addStyleRule(node, (CSSStyleRule) rule);
+            def styleName = rule.getSelectorText()
+            def style = ((CSSStyleRule) rule).getStyle();
+            if (style) {
+//                        println styleName
+                def styles = []
+                int len = style.getLength()
+                for (int j = 0; j < len; j++) {
+                    String name = style.item(j);
+                    String value = style.getPropertyValue(name);
+                    String prio = style.getPropertyPriority(name);
+
+                    if (prio.equals("")) {
+                        styles << ["$name": "$value"]
+                    } else {
+                        styles << "$name : $value ! $prio)"
+                    }
+                }
+
+                styleName.split(",").each {
+                    listing.put(it.trim(), styles)
+                }
+            }
+//                    println "CSSStyleRule"
+            break;
+        case CSSRule.CHARSET_RULE:
+//                addCharsetRule(node, (CSSCharsetRule) rule);
+            println "CSSCharsetRule"
+            break;
+        case CSSRule.IMPORT_RULE:
+//                addImportRule(node, (CSSImportRule) rule);
+            println "CSSImportRule"
+            break;
+        case CSSRule.MEDIA_RULE:
+//                addMediaRule(node, (CSSMediaRule) rule);
+            println "CSSMediaRule"
+            break;
+        case CSSRule.FONT_FACE_RULE:
+//                addFontFaceRule(node, (CSSFontFaceRule) rule);
+            println "CSSFontFaceRule"
+            break;
+        case CSSRule.PAGE_RULE:
+//                addPageRule(node, (CSSPageRule) rule);
+            println "CSSPageRule"
+            break;
     }
+
+}
+
+listing.each {
+    println it
 }
