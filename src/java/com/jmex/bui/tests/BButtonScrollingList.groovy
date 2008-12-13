6db60ee183554cdbb50cfded14b12a3eb1c43f05
standtrooper <standtrooper@bc7b2f82-3230-0410-b470-b99ffb12e3d1>
2008-12-13 18:50:45 +0000

    examples for showing the difference between removeValues() and removeAll()

---
+++ src/java/com/jmex/bui/tests/BButtonScrollingList.groovy
@@ -0,0 +1,20 @@
+/**
+ * $ID$
+ * $COPYRIGHT$
+ */
+package com.jmex.bui.tests
+
+import com.jmex.bui.BButton
+import com.jmex.bui.BScrollingList
+
+/**
+ *
+ * @author timo
+ * @since Dec 13, 2008 12:19:51 PM
+ */
+class BButtonScrollingList extends BScrollingList {
+    @Override
+    public BButton createComponent(Object str) {
+        return new BButton((String) str);
+    }
+}
