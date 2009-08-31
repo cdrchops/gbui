e04ae005e33c7e8c3003d8597b82b15591ba4145
standtrooper <standtrooper@bc7b2f82-3230-0410-b470-b99ffb12e3d1>
2009-08-31 19:39:03 +0000

    modified to check changes for this "bug"

--- test/java/com/jmex/bui/ButtonWiggleTest.java
+++ test/java/com/jmex/bui/ButtonWiggleTest.java
@@ -9,19 +9,10 @@ package com.jmex.bui;
  * @since Aug 31, 2009 - 10:39:28 AM
  */
 
-import java.io.IOException;
-import java.net.URL;
-
 import com.jme.app.SimpleGame;
 import com.jme.input.MouseInput;
 import com.jme.math.FastMath;
 import com.jme.util.Timer;
-import com.jmex.bui.BButton;
-import com.jmex.bui.BComponent;
-import com.jmex.bui.BImage;
-import com.jmex.bui.BWindow;
-import com.jmex.bui.BuiSystem;
-import com.jmex.bui.PolledRootNode;
 import com.jmex.bui.background.ImageBackground;
 import com.jmex.bui.enumeratedConstants.ImageBackgroundMode;
 import com.jmex.bui.event.ActionEvent;
@@ -29,26 +20,29 @@ import com.jmex.bui.event.ActionListener;
 import com.jmex.bui.layout.AbsoluteLayout;
 import com.jmex.bui.util.Point;
 
+import java.io.IOException;
+import java.net.URL;
+
 public class ButtonWiggleTest extends SimpleGame {
 
-	public static void main(String[] args) {
-		ButtonWiggleTest app = new ButtonWiggleTest();
-		app.setConfigShowMode(ConfigShowMode.AlwaysShow);
-		app.start();
-	}
+    public static void main(String[] args) {
+        ButtonWiggleTest app = new ButtonWiggleTest();
+        app.setConfigShowMode(ConfigShowMode.AlwaysShow);
+        app.start();
+    }
 
-	float transitionPosition = 1f;
-	Point startPosition = new Point(270, 190);
-	BWindow bWindow;
-	BButton bButton;
+    float transitionPosition = 1f;
+    Point startPosition = new Point(270, 190);
+    BWindow bWindow;
+    BButton bButton;
 
-	protected void simpleInitGame() {
+    protected void simpleInitGame() {
         MouseInput.get().setCursorVisible(true);
-		BuiSystem.init(new PolledRootNode(Timer.getTimer(), input), "/rsrc/style2.bss");
-    	rootNode.attachChild(BuiSystem.getRootNode());
-		bWindow = new BWindow(BuiSystem.getStyle(), new AbsoluteLayout());
-		bButton = new BButton("Click Me!");
-		bButton.setPreferredSize(100, 100);
+        BuiSystem.init(new PolledRootNode(Timer.getTimer(), input), "/rsrc/style2.bss");
+        rootNode.attachChild(BuiSystem.getRootNode());
+        bWindow = new BWindow(BuiSystem.getStyle(), new AbsoluteLayout());
+        bButton = new BButton("Click Me!");
+        bButton.setPreferredSize(100, 100);
         bButton.addListener(new ActionListener() {
             public void actionPerformed(ActionEvent event) {
                 System.out.println("Button Pressed!");
@@ -59,52 +53,51 @@ public class ButtonWiggleTest extends SimpleGame {
         bWindow.add(bButton, new Point(270, 190));
         BImage image = null;
         URL url = null;
-		try {
-            url = getClass().getClassLoader().getResource("rsrc/inner_darkblue_4.png");
+        try {
+            url = getClass().getClassLoader().getResource("jmetest/data/texture/Detail.jpg");
             image = new BImage(url);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         bWindow.setBackground(BComponent.DEFAULT, new ImageBackground(ImageBackgroundMode.SCALE_XY, image));
         BuiSystem.addWindow(bWindow);
         bWindow.center();
+    }
 
-	}
+    public void simpleUpdate() {
+        updateTransition(super.tpf, 10f, -1);
 
-	public void simpleUpdate() {
-		updateTransition(super.tpf, 10f, -1);
-
-        float transitionOffset = (float)FastMath.pow(transitionPosition, 2);
+        float transitionOffset = (float) FastMath.pow(transitionPosition, 2);
 
         int tempPosition = (int) startPosition.x;
 
         tempPosition -= transitionOffset * 512;
 
-       	bButton.setBounds(
-        			tempPosition, bButton.getY(),
-        			bButton.getWidth(), bButton.getHeight());
+        bButton.setBounds(tempPosition,
+                          bButton.getY(),
+                          bButton.getWidth(),
+                          bButton.getHeight());
 
-		super.simpleUpdate();
-	}
+        super.simpleUpdate();
+    }
 
-    boolean updateTransition(float tpf, float transitionOffTime, int direction)
-    {
+    boolean updateTransition(float tpf, float transitionOffTime, int direction) {
         // How much should we move by?
         float transitionDelta;
 
-        if (transitionOffTime == 0)
+        if (transitionOffTime == 0) {
             transitionDelta = 1;
-        else
+        } else {
             transitionDelta = tpf;
+        }
 
         // Update the transition position.
         transitionPosition += transitionDelta * direction;
 
         // Did we reach the end of the transition?
         if (((direction < 0) && (transitionPosition <= 0)) ||
-            ((direction > 0) && (transitionPosition >= 1)))
-        {
+            ((direction > 0) && (transitionPosition >= 1))) {
             transitionPosition = FastMath.clamp(transitionPosition, 0, 1);
             return false;
         }
@@ -112,5 +105,4 @@ public class ButtonWiggleTest extends SimpleGame {
         // Otherwise we are still busy transitioning.
         return true;
     }
-
 }
