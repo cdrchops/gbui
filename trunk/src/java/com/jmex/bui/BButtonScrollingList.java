da0f842f45cceffe72bf0723a73fa2c148fc9980
standtrooper <standtrooper@bc7b2f82-3230-0410-b470-b99ffb12e3d1>
2009-09-24 19:46:26 +0000

    removed @Override from implemented methods

--- trunk/src/java/com/jmex/bui/BButtonScrollingList.java
+++ trunk/src/java/com/jmex/bui/BButtonScrollingList.java
@@ -9,7 +9,6 @@ package com.jmex.bui;
  * @since Mar 24, 2009 - 10:37:43 AM
  */
 public class BButtonScrollingList extends BScrollingList {
-    @Override
     public BButton createComponent(Object str) {
         return new BButton((String) str);
     }
