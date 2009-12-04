4cc7bcc5eb5526521f079b8b0fe321496adba8d7
Captain.queezy@gmail.com <Captain.queezy@gmail.com@bc7b2f82-3230-0410-b470-b99ffb12e3d1>
2009-12-04 14:06:27 +0000

    - Added focus listener support to BTextField

---
+++ trunk/test/java/com/jmex/bui/AllAutomatedTests.java
@@ -0,0 +1,13 @@
+package com.jmex.bui;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@SuiteClasses ( {
+	BTextFieldTest.class
+})
+@RunWith(Suite.class)
+public class AllAutomatedTests {
+
+}
