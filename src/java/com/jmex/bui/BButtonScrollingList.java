ab56e1b8f4aaf2d35758f82782c74f07c3cea024
standtrooper <standtrooper@bc7b2f82-3230-0410-b470-b99ffb12e3d1>
2010-01-18 21:08:31 +0000

    added copyright and ID with the values to replace on check ins

--- src/java/com/jmex/bui/BButtonScrollingList.java
+++ src/java/com/jmex/bui/BButtonScrollingList.java
@@ -8,7 +8,7 @@ package com.jmex.bui;
  * @author torr
  * @since Mar 24, 2009 - 10:37:43 AM
  */
-public class BButtonScrollingList<V, C extends BComponent> extends BScrollingList<V, C> {
+public class BButtonScrollingList extends BScrollingList {
     @Override
     public BComponent createComponent(final Object value) {
         return new BButton((String) value);
