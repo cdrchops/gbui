a3c24246b737c66a75671c680fe0d1e5840f027f
schizosfera <schizosfera@bc7b2f82-3230-0410-b470-b99ffb12e3d1>
2010-01-18 21:16:02 +0000

    -added license header -changed the class definition to reflect the types it's actually using

--- src/java/com/jmex/bui/BButtonScrollingList.java
+++ src/java/com/jmex/bui/BButtonScrollingList.java
@@ -1,6 +1,25 @@
 /**
- * $ID:$
- * $COPYRIGHT:$
+ *
+ * $Id
+ * $Copyright
+ *
+ * BUI - a user interface library for the JME 3D engine
+ * Copyright (C) 2005-2006, Michael Bayne, All Rights Reserved
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
  */
 package com.jmex.bui;
 
@@ -8,9 +27,9 @@ package com.jmex.bui;
  * @author torr
  * @since Mar 24, 2009 - 10:37:43 AM
  */
-public class BButtonScrollingList extends BScrollingList {
+public class BButtonScrollingList extends BScrollingList<String, BButton> {
     @Override
-    public BComponent createComponent(final Object value) {
-        return new BButton((String) value);
+    public BButton createComponent(final String value) {
+        return new BButton(value);
     }
 }
